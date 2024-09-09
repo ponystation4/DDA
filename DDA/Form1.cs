c9f5d1893f593be8e7bd52dd5f175a7fa54f6330
ponystation4 <ponystation4@gmail.com>
2024-09-09 03:03:36 -0600

    Awaken, my dear C gato, for thine is the time yet again to start plotting once more. It has begun and it has been released

--- DDA/Form1.cs
+++ DDA/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,18 @@ namespace DDA
 {
     public partial class Form1 : Form
     {
+        // Create an instance of a FormsPlot like this
+        readonly FormsPlot FormsPlot1 = new FormsPlot() { Dock = DockStyle.Fill };
         public Form1()
         {
             InitializeComponent();
+            // Add the FormsPlot to the panel
+            panel1.Controls.Add(FormsPlot1);
+
+            // Plot data using the control
+            double[] data = ScottPlot.Generate.Sin();
+            FormsPlot1.Plot.Add.Signal(data);
+            FormsPlot1.Refresh();
         }
     }
 }
