62726014dece9ef34915f38deabaed493e378b83
ponystation4 <ponystation4@gmail.com>
2024-09-10 22:23:32 -0600

    Entrada del usuario + despliegue

--- DDA/Form1.Designer.cs
+++ DDA/Form1.Designer.cs
@@ -28,128 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.lbl_x1 = new System.Windows.Forms.Label();
+            this.lbl_y1 = new System.Windows.Forms.Label();
+            this.txt_x1 = new System.Windows.Forms.TextBox();
+            this.txt_y1 = new System.Windows.Forms.TextBox();
+            this.txt_x2 = new System.Windows.Forms.TextBox();
+            this.txt_y2 = new System.Windows.Forms.TextBox();
+            this.lbl_x2 = new System.Windows.Forms.Label();
+            this.lbl_y2 = new System.Windows.Forms.Label();
+            this.dgv_tabla = new System.Windows.Forms.DataGridView();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.lbl_Elem = new System.Windows.Forms.Label();
+            this.txt_Elem = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_tabla)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.panel1.Location = new System.Drawing.Point(232, 12);
+            this.panel1.Location = new System.Drawing.Point(246, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(431, 332);
+            this.panel1.Size = new System.Drawing.Size(417, 332);
             this.panel1.TabIndex = 0;
             // 
-            // button1
+            // btn_calc
             // 
-            this.button1.Location = new System.Drawing.Point(12, 139);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 56);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_calc.Location = new System.Drawing.Point(12, 139);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(99, 56);
+            this.btn_calc.TabIndex = 1;
+            this.btn_calc.Text = "Calcular";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
             // 
-            // label1
+            // lbl_x1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.lbl_x1.AutoSize = true;
+            this.lbl_x1.Location = new System.Drawing.Point(12, 13);
+            this.lbl_x1.Name = "lbl_x1";
+            this.lbl_x1.Size = new System.Drawing.Size(23, 13);
+            this.lbl_x1.TabIndex = 2;
+            this.lbl_x1.Text = "X1:";
             // 
-            // label2
+            // lbl_y1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(135, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.lbl_y1.AutoSize = true;
+            this.lbl_y1.Location = new System.Drawing.Point(65, 13);
+            this.lbl_y1.Name = "lbl_y1";
+            this.lbl_y1.Size = new System.Drawing.Size(23, 13);
+            this.lbl_y1.TabIndex = 3;
+            this.lbl_y1.Text = "Y1:";
             // 
-            // textBox1
+            // txt_x1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 29);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(71, 20);
-            this.textBox1.TabIndex = 4;
+            this.txt_x1.Location = new System.Drawing.Point(12, 29);
+            this.txt_x1.Name = "txt_x1";
+            this.txt_x1.Size = new System.Drawing.Size(50, 20);
+            this.txt_x1.TabIndex = 4;
             // 
-            // textBox2
+            // txt_y1
             // 
-            this.textBox2.Location = new System.Drawing.Point(138, 29);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(71, 20);
-            this.textBox2.TabIndex = 5;
+            this.txt_y1.Location = new System.Drawing.Point(68, 29);
+            this.txt_y1.Name = "txt_y1";
+            this.txt_y1.Size = new System.Drawing.Size(50, 20);
+            this.txt_y1.TabIndex = 5;
             // 
-            // textBox3
+            // txt_x2
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 95);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(71, 20);
-            this.textBox3.TabIndex = 9;
+            this.txt_x2.Location = new System.Drawing.Point(12, 80);
+            this.txt_x2.Name = "txt_x2";
+            this.txt_x2.Size = new System.Drawing.Size(50, 20);
+            this.txt_x2.TabIndex = 9;
             // 
-            // textBox4
+            // txt_y2
             // 
-            this.textBox4.Location = new System.Drawing.Point(138, 95);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(71, 20);
-            this.textBox4.TabIndex = 8;
+            this.txt_y2.Location = new System.Drawing.Point(68, 80);
+            this.txt_y2.Name = "txt_y2";
+            this.txt_y2.Size = new System.Drawing.Size(50, 20);
+            this.txt_y2.TabIndex = 8;
             // 
-            // label3
+            // lbl_x2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 79);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "label3";
+            this.lbl_x2.AutoSize = true;
+            this.lbl_x2.Location = new System.Drawing.Point(12, 64);
+            this.lbl_x2.Name = "lbl_x2";
+            this.lbl_x2.Size = new System.Drawing.Size(23, 13);
+            this.lbl_x2.TabIndex = 7;
+            this.lbl_x2.Text = "X2:";
             // 
-            // label4
+            // lbl_y2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(135, 79);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
+            this.lbl_y2.AutoSize = true;
+            this.lbl_y2.Location = new System.Drawing.Point(65, 64);
+            this.lbl_y2.Name = "lbl_y2";
+            this.lbl_y2.Size = new System.Drawing.Size(23, 13);
+            this.lbl_y2.TabIndex = 6;
+            this.lbl_y2.Text = "Y2:";
             // 
-            // dataGridView1
+            // dgv_tabla
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 201);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(211, 143);
-            this.dataGridView1.TabIndex = 10;
+            this.dgv_tabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.NullValue = null;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv_tabla.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_tabla.Location = new System.Drawing.Point(12, 201);
+            this.dgv_tabla.Name = "dgv_tabla";
+            this.dgv_tabla.Size = new System.Drawing.Size(228, 143);
+            this.dgv_tabla.TabIndex = 10;
+            this.dgv_tabla.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgv_tabla_CellFormatting);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.Location = new System.Drawing.Point(141, 139);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(99, 56);
+            this.btn_Limpiar.TabIndex = 11;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = true;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // lbl_Elem
+            // 
+            this.lbl_Elem.AutoSize = true;
+            this.lbl_Elem.Location = new System.Drawing.Point(127, 36);
+            this.lbl_Elem.Name = "lbl_Elem";
+            this.lbl_Elem.Size = new System.Drawing.Size(113, 13);
+            this.lbl_Elem.TabIndex = 12;
+            this.lbl_Elem.Text = "Número de elementos:";
+            // 
+            // txt_Elem
+            // 
+            this.txt_Elem.Location = new System.Drawing.Point(147, 57);
+            this.txt_Elem.Name = "txt_Elem";
+            this.txt_Elem.Size = new System.Drawing.Size(75, 20);
+            this.txt_Elem.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 356);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_Elem);
+            this.Controls.Add(this.lbl_Elem);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.dgv_tabla);
+            this.Controls.Add(this.txt_x2);
+            this.Controls.Add(this.txt_y2);
+            this.Controls.Add(this.lbl_x2);
+            this.Controls.Add(this.lbl_y2);
+            this.Controls.Add(this.txt_y1);
+            this.Controls.Add(this.txt_x1);
+            this.Controls.Add(this.lbl_y1);
+            this.Controls.Add(this.lbl_x1);
+            this.Controls.Add(this.btn_calc);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_tabla)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,16 +203,19 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Label lbl_x1;
+        private System.Windows.Forms.Label lbl_y1;
+        private System.Windows.Forms.TextBox txt_x1;
+        private System.Windows.Forms.TextBox txt_y1;
+        private System.Windows.Forms.TextBox txt_x2;
+        private System.Windows.Forms.TextBox txt_y2;
+        private System.Windows.Forms.Label lbl_x2;
+        private System.Windows.Forms.Label lbl_y2;
+        private System.Windows.Forms.DataGridView dgv_tabla;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Label lbl_Elem;
+        private System.Windows.Forms.TextBox txt_Elem;
     }
 }
 
--- DDA/Form1.cs
+++ DDA/Form1.cs
@@ -1,4 +1,5 @@
-﻿using ScottPlot.WinForms;
+﻿using ScottPlot.TickGenerators.TimeUnits;
+using ScottPlot.WinForms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,20 +14,93 @@ namespace DDA
 {
     public partial class Form1 : Form
     {
-        // Create an instance of a FormsPlot like this
+        // Se crea una instancia de FormsPlot porque no jala en Diseño
         readonly FormsPlot FormsPlot1 = new FormsPlot() { Dock = DockStyle.Fill };
         public Form1()
         {
             InitializeComponent();
-            // Add the FormsPlot to the panel
+            // Añade el FormsPlot de ScottPlot al panel
             panel1.Controls.Add(FormsPlot1);
-
-            // Plot data using the control
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY = { 1, 2, 3, 4, 5 };
-
-            FormsPlot1.Plot.Add.Scatter(dataX, dataY);
             FormsPlot1.Refresh();
+            //Se crean las 2 columnas del DataGridView
+            dgv_tabla.Columns.Add("Column1", "X");
+            dgv_tabla.Columns.Add("Column2", "Y");
+            FormsPlot1.Plot.Title("Preparado.");
+        }
+
+        void Limpiar()
+        {
+            txt_x1.Clear();
+            txt_x2.Clear();
+            txt_y1.Clear();
+            txt_y2.Clear();
+            txt_Elem.Clear();
+            FormsPlot1.Plot.Clear();
+            FormsPlot1.Refresh();
+            dgv_tabla.Rows.Clear();
+            FormsPlot1.Plot.Title("Preparado.");
+        }
+
+        private void btn_calc_Click(object sender, EventArgs e)
+        {
+            FormsPlot1.Plot.Clear();
+            dgv_tabla.Rows.Clear();
+            //Obtener los valores de los textboxes
+            double x1 = Convert.ToDouble(txt_x1.Text);
+            double x2 = Convert.ToDouble(txt_x2.Text);
+            double y1 = Convert.ToDouble(txt_y1.Text);
+            double y2 = Convert.ToDouble(txt_y2.Text);
+            int elem = Convert.ToInt32(txt_Elem.Text);
+
+            //Crear arrays con los valores de X y Y y los elementos intermedios
+            double[] x_valores = new double[elem];
+            double[] y_valores = new double[elem];
+
+            //Creación de tamaño de intervalos para x2-x1 y y2-y1
+            double x_intervalo = (x2 - x1) / (elem - 1);
+            double y_intervalo = (y2 - y1) / (elem - 1);
+
+            //Se crean los valores para los arrays y se agregan al DataGridView
+            for (int i = 0; i < elem; i++)
+            {
+                x_valores[i] = x1 + i * x_intervalo;
+                y_valores[i] = y1 + i * y_intervalo;
+                dgv_tabla.Rows.Add();
+                dgv_tabla.Rows[i].Cells[0].Value = x_valores[i];
+                dgv_tabla.Rows[i].Cells[1].Value = x_valores[i];
+            }
+
+            //Se le mandan a FormsPlot1 los valores y los muestra en pantalla
+            FormsPlot1.Plot.Add.Scatter(x_valores, y_valores);
+            FormsPlot1.Plot.Axes.AutoScale();
+            FormsPlot1.Plot.Title("(" + Convert.ToString(x_valores[0]) + ", " + Convert.ToString(y_valores[0]) + ") -> (" + Convert.ToString(x_valores[elem-1]) + ", " + Convert.ToString(y_valores[elem-1]) + "), " + Convert.ToString(elem) + " elementos.");
+            FormsPlot1.Refresh();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        //Dar formato a las celdas con decimales
+        private void dgv_tabla_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.Value != null && e.Value is double)
+            {
+                double valor = (double)e.Value;
+
+                // Si el valor tiene decimales, lo mostramos con 2 decimales, de lo contrario sin decimales
+                if (valor % 1 == 0)
+                {
+                    e.Value = valor.ToString("N0"); // Sin decimales
+                }
+                else
+                {
+                    e.Value = valor.ToString("N5"); // Con 2 decimales
+                }
+
+                e.FormattingApplied = true;
+            }
         }
     }
 }
