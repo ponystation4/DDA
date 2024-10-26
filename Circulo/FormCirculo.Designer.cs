22051ce2a8ec232b690706d9446e29758d94452e
ponystation4 <ponystation4@gmail.com>
2024-10-26 17:42:56 -0600

    Círculo

---
+++ Circulo/FormCirculo.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Circulo
+{
+    partial class FormCirculo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgv_1 = new System.Windows.Forms.DataGridView();
+            this.txt_X = new System.Windows.Forms.TextBox();
+            this.txt_Y = new System.Windows.Forms.TextBox();
+            this.lbl_Coord = new System.Windows.Forms.Label();
+            this.lbl_X = new System.Windows.Forms.Label();
+            this.lbl_Y = new System.Windows.Forms.Label();
+            this.lbl_Radio = new System.Windows.Forms.Label();
+            this.txt_Radio = new System.Windows.Forms.TextBox();
+            this.dgv_2 = new System.Windows.Forms.DataGridView();
+            this.dgv_3 = new System.Windows.Forms.DataGridView();
+            this.dgv_4 = new System.Windows.Forms.DataGridView();
+            this.dgv_5 = new System.Windows.Forms.DataGridView();
+            this.dgv_6 = new System.Windows.Forms.DataGridView();
+            this.dgv_7 = new System.Windows.Forms.DataGridView();
+            this.dgv_8 = new System.Windows.Forms.DataGridView();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Location = new System.Drawing.Point(618, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(426, 426);
+            this.panel1.TabIndex = 22;
+            // 
+            // dgv_1
+            // 
+            this.dgv_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_1.Location = new System.Drawing.Point(12, 132);
+            this.dgv_1.Name = "dgv_1";
+            this.dgv_1.Size = new System.Drawing.Size(130, 150);
+            this.dgv_1.TabIndex = 23;
+            // 
+            // txt_X
+            // 
+            this.txt_X.Location = new System.Drawing.Point(72, 69);
+            this.txt_X.Name = "txt_X";
+            this.txt_X.Size = new System.Drawing.Size(60, 20);
+            this.txt_X.TabIndex = 24;
+            // 
+            // txt_Y
+            // 
+            this.txt_Y.Location = new System.Drawing.Point(138, 69);
+            this.txt_Y.Name = "txt_Y";
+            this.txt_Y.Size = new System.Drawing.Size(60, 20);
+            this.txt_Y.TabIndex = 25;
+            // 
+            // lbl_Coord
+            // 
+            this.lbl_Coord.AutoSize = true;
+            this.lbl_Coord.Location = new System.Drawing.Point(72, 49);
+            this.lbl_Coord.Name = "lbl_Coord";
+            this.lbl_Coord.Size = new System.Drawing.Size(115, 13);
+            this.lbl_Coord.TabIndex = 26;
+            this.lbl_Coord.Text = "Coordenadas de inicio:";
+            // 
+            // lbl_X
+            // 
+            this.lbl_X.AutoSize = true;
+            this.lbl_X.Location = new System.Drawing.Point(118, 92);
+            this.lbl_X.Name = "lbl_X";
+            this.lbl_X.Size = new System.Drawing.Size(14, 13);
+            this.lbl_X.TabIndex = 27;
+            this.lbl_X.Text = "X";
+            // 
+            // lbl_Y
+            // 
+            this.lbl_Y.AutoSize = true;
+            this.lbl_Y.Location = new System.Drawing.Point(184, 92);
+            this.lbl_Y.Name = "lbl_Y";
+            this.lbl_Y.Size = new System.Drawing.Size(14, 13);
+            this.lbl_Y.TabIndex = 28;
+            this.lbl_Y.Text = "Y";
+            // 
+            // lbl_Radio
+            // 
+            this.lbl_Radio.AutoSize = true;
+            this.lbl_Radio.Location = new System.Drawing.Point(246, 49);
+            this.lbl_Radio.Name = "lbl_Radio";
+            this.lbl_Radio.Size = new System.Drawing.Size(38, 13);
+            this.lbl_Radio.TabIndex = 29;
+            this.lbl_Radio.Text = "Radio:";
+            // 
+            // txt_Radio
+            // 
+            this.txt_Radio.Location = new System.Drawing.Point(249, 69);
+            this.txt_Radio.Name = "txt_Radio";
+            this.txt_Radio.Size = new System.Drawing.Size(50, 20);
+            this.txt_Radio.TabIndex = 30;
+            // 
+            // dgv_2
+            // 
+            this.dgv_2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_2.Location = new System.Drawing.Point(150, 132);
+            this.dgv_2.Name = "dgv_2";
+            this.dgv_2.Size = new System.Drawing.Size(130, 150);
+            this.dgv_2.TabIndex = 31;
+            // 
+            // dgv_3
+            // 
+            this.dgv_3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_3.Location = new System.Drawing.Point(286, 132);
+            this.dgv_3.Name = "dgv_3";
+            this.dgv_3.Size = new System.Drawing.Size(130, 150);
+            this.dgv_3.TabIndex = 32;
+            // 
+            // dgv_4
+            // 
+            this.dgv_4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_4.Location = new System.Drawing.Point(422, 132);
+            this.dgv_4.Name = "dgv_4";
+            this.dgv_4.Size = new System.Drawing.Size(130, 150);
+            this.dgv_4.TabIndex = 33;
+            // 
+            // dgv_5
+            // 
+            this.dgv_5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_5.Location = new System.Drawing.Point(12, 288);
+            this.dgv_5.Name = "dgv_5";
+            this.dgv_5.Size = new System.Drawing.Size(130, 150);
+            this.dgv_5.TabIndex = 37;
+            // 
+            // dgv_6
+            // 
+            this.dgv_6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_6.Location = new System.Drawing.Point(150, 288);
+            this.dgv_6.Name = "dgv_6";
+            this.dgv_6.Size = new System.Drawing.Size(130, 150);
+            this.dgv_6.TabIndex = 36;
+            // 
+            // dgv_7
+            // 
+            this.dgv_7.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_7.Location = new System.Drawing.Point(286, 288);
+            this.dgv_7.Name = "dgv_7";
+            this.dgv_7.Size = new System.Drawing.Size(130, 150);
+            this.dgv_7.TabIndex = 35;
+            // 
+            // dgv_8
+            // 
+            this.dgv_8.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_8.Location = new System.Drawing.Point(422, 288);
+            this.dgv_8.Name = "dgv_8";
+            this.dgv_8.Size = new System.Drawing.Size(130, 150);
+            this.dgv_8.TabIndex = 34;
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calcular.Location = new System.Drawing.Point(326, 58);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(75, 39);
+            this.btn_Calcular.TabIndex = 38;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(431, 58);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(75, 39);
+            this.btn_Limpiar.TabIndex = 39;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = true;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // FormCirculo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 450);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.dgv_5);
+            this.Controls.Add(this.dgv_6);
+            this.Controls.Add(this.dgv_7);
+            this.Controls.Add(this.dgv_8);
+            this.Controls.Add(this.dgv_4);
+            this.Controls.Add(this.dgv_3);
+            this.Controls.Add(this.dgv_2);
+            this.Controls.Add(this.txt_Radio);
+            this.Controls.Add(this.lbl_Radio);
+            this.Controls.Add(this.lbl_Y);
+            this.Controls.Add(this.lbl_X);
+            this.Controls.Add(this.lbl_Coord);
+            this.Controls.Add(this.txt_Y);
+            this.Controls.Add(this.txt_X);
+            this.Controls.Add(this.dgv_1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormCirculo";
+            this.Text = "Círculo";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgv_1;
+        private System.Windows.Forms.TextBox txt_X;
+        private System.Windows.Forms.TextBox txt_Y;
+        private System.Windows.Forms.Label lbl_Coord;
+        private System.Windows.Forms.Label lbl_X;
+        private System.Windows.Forms.Label lbl_Y;
+        private System.Windows.Forms.Label lbl_Radio;
+        private System.Windows.Forms.TextBox txt_Radio;
+        private System.Windows.Forms.DataGridView dgv_2;
+        private System.Windows.Forms.DataGridView dgv_3;
+        private System.Windows.Forms.DataGridView dgv_4;
+        private System.Windows.Forms.DataGridView dgv_5;
+        private System.Windows.Forms.DataGridView dgv_6;
+        private System.Windows.Forms.DataGridView dgv_7;
+        private System.Windows.Forms.DataGridView dgv_8;
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Button btn_Limpiar;
+    }
+}
+
---
+++ Circulo/FormCirculo.cs
@@ -0,0 +1,283 @@
+﻿using ScottPlot;
+using ScottPlot.Palettes;
+using ScottPlot.Plottables;
+using ScottPlot.TickGenerators.TimeUnits;
+using ScottPlot.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circulo
+{
+    public partial class FormCirculo : Form
+    {
+        //Lista de valores final
+        List<double> listaColumnaX = new List<double>();
+        List<double> listaColumnaY = new List<double>();
+
+        // Se crea una instancia de FormsPlot porque no jala en Diseño
+        readonly FormsPlot FormsPlot1 = new FormsPlot() { Dock = DockStyle.Fill };
+        public FormCirculo()
+        {
+            InitializeComponent();
+            // Añade el FormsPlot de ScottPlot al panel
+            panel1.Controls.Add(FormsPlot1);
+            //Se indica que la gráfica está preparada
+            FormsPlot1.Plot.Title("Preparado.");
+            //Se crean las listas de valores de los octantes
+            dgv_1.Columns.Add("Column1", "X");
+            dgv_1.Columns.Add("Column2", "Y");
+            dgv_2.Columns.Add("Column1", "X");
+            dgv_2.Columns.Add("Column2", "Y");
+            dgv_3.Columns.Add("Column1", "X");
+            dgv_3.Columns.Add("Column2", "Y");
+            dgv_4.Columns.Add("Column1", "X");
+            dgv_4.Columns.Add("Column2", "Y");
+            dgv_5.Columns.Add("Column1", "X");
+            dgv_5.Columns.Add("Column2", "Y");
+            dgv_6.Columns.Add("Column1", "X");
+            dgv_6.Columns.Add("Column2", "Y");
+            dgv_7.Columns.Add("Column1", "X");
+            dgv_7.Columns.Add("Column2", "Y");
+            dgv_8.Columns.Add("Column1", "X");
+            dgv_8.Columns.Add("Column2", "Y");
+        }
+        void LimpiarDGV()
+        {
+            dgv_1.Rows.Clear();
+            dgv_2.Rows.Clear();
+            dgv_3.Rows.Clear();
+            dgv_4.Rows.Clear();
+            dgv_5.Rows.Clear();
+            dgv_6.Rows.Clear();
+            dgv_7.Rows.Clear();
+            dgv_8.Rows.Clear();
+        }
+        void LimpiarPlot()
+        {
+            FormsPlot1.Plot.Clear();
+            FormsPlot1.Plot.Title("Preparado.");
+            FormsPlot1.Refresh();
+        }
+        void LimpiarTexto()
+        {
+            txt_Radio.Clear();
+            txt_X.Clear();
+            txt_Y.Clear();
+        }
+        void Limpiar()
+        {
+            LimpiarDGV();
+            LimpiarPlot();
+            LimpiarTexto();
+        }
+
+        private void UnirValoresColumnas(DataGridView dgv)
+        {
+            
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                // Verificar que la fila no esté vacía
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null)
+                {
+                    // Agregar los valores de cada columna a sus respectivas listas
+                    listaColumnaX.Add(Convert.ToDouble(row.Cells[0].Value));
+                    listaColumnaY.Add(Convert.ToDouble(row.Cells[1].Value));
+                }
+            }
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            double radio = Convert.ToDouble(txt_Radio.Text);
+            double x = Convert.ToDouble(txt_X.Text);
+            double y = Convert.ToDouble(txt_Y.Text);
+
+            int N = 0;
+            double PK = 0;
+            double nPK = 0; //Pk+1
+            double K = 0;
+
+            //Parámetro de decisión
+            PK = 1 - radio;
+
+            //Se crean las listas de valores
+            List<double> listaX = new List<double>();
+            List<double> listaY = new List<double>();
+            
+
+            //Por si acaso limpiar las listas y reducir el tamaño a 0
+            listaX.Clear();
+            listaX.TrimExcess();
+            listaY.Clear();
+            listaY.TrimExcess();
+            listaColumnaX.Clear();
+            listaColumnaX.TrimExcess();
+            listaColumnaY.Clear();
+            listaColumnaY.TrimExcess();
+
+            //Limpiar tablas y FormsPlot
+            FormsPlot1.Plot.Clear();
+            dgv_1.Rows.Clear();
+            dgv_2.Rows.Clear();
+            dgv_3.Rows.Clear();
+            dgv_4.Rows.Clear();
+            dgv_5.Rows.Clear();
+            dgv_6.Rows.Clear();
+            dgv_7.Rows.Clear();
+            dgv_8.Rows.Clear();
+
+            //Se añade el primer valor
+            double xsum = 0;
+            double ysum = radio;
+
+            //Obtención de valores
+            do
+            {
+                if (N == 0)
+                {
+                    listaX.Add(xsum);
+                    listaY.Add(radio);
+                    xsum++;
+                    nPK = PK;
+                }
+                else
+                {
+                    if (nPK < 0)
+                    {
+                        PK = nPK;
+                        nPK = PK + (2 * xsum) + 1;
+                        listaX.Add(xsum);
+                        listaY.Add(ysum);
+                        xsum++;
+                    }
+                    else //if (nPK => 0)
+                    {
+                        PK = nPK;
+                        nPK = PK + (2 * xsum) + 1 - (2 * ysum);
+                        listaX.Add(xsum);
+                        xsum++;
+                        listaY.Add(ysum);
+                        ysum--;
+                    }
+
+                }
+
+
+                N++;
+            }
+            while (!(xsum >= ysum));
+
+            double[] ValoresX = listaX.ToArray();
+            double[] ValoresY = listaY.ToArray();
+
+            double[] ValoresXNeg = new double[ValoresX.Length];
+            for (int i = 0; i < ValoresX.Length; i++)
+            {
+                ValoresXNeg[i] = -ValoresX[i]; // Asigna el valor negativo de cada elemento
+            }
+
+            double[] ValoresYNeg = new double[ValoresY.Length];
+            for (int i = 0; i < ValoresY.Length; i++)
+            {
+                ValoresYNeg[i] = -ValoresY[i]; // Asigna el valor negativo de cada elemento
+            }
+
+            for (int i = 0; i < ValoresX.Length; i++) //Añade los valores a las tablas
+            {
+                dgv_1.Rows.Add();
+                dgv_1.Rows[i].Cells[0].Value = ValoresX[i] + x;
+                dgv_1.Rows[i].Cells[1].Value = ValoresY[i] + y;
+                dgv_1.AutoResizeColumns();
+                dgv_1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                dgv_2.Rows.Add();
+                dgv_2.Rows[i].Cells[0].Value = ValoresY[i] + x;
+                dgv_2.Rows[i].Cells[1].Value = ValoresX[i] + y;
+                dgv_2.AutoResizeColumns();
+                dgv_2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                dgv_3.Rows.Add();
+                dgv_3.Rows[i].Cells[0].Value = ValoresYNeg[i] + x;
+                dgv_3.Rows[i].Cells[1].Value = ValoresX[i] + y;
+                dgv_3.AutoResizeColumns();
+                dgv_3.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                dgv_4.Rows.Add();
+                dgv_4.Rows[i].Cells[0].Value = ValoresX[i] + x;
+                dgv_4.Rows[i].Cells[1].Value = ValoresYNeg[i] + y;
+                dgv_4.AutoResizeColumns();
+                dgv_4.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                dgv_5.Rows.Add();
+                dgv_5.Rows[i].Cells[0].Value = ValoresYNeg[i] + x;
+                dgv_5.Rows[i].Cells[1].Value = ValoresXNeg[i] + y;
+                dgv_5.AutoResizeColumns();
+                dgv_5.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                dgv_6.Rows.Add();
+                dgv_6.Rows[i].Cells[0].Value = ValoresXNeg[i] + x;
+                dgv_6.Rows[i].Cells[1].Value = ValoresYNeg[i] + y;
+                dgv_6.AutoResizeColumns();
+                dgv_6.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                dgv_7.Rows.Add();
+                dgv_7.Rows[i].Cells[0].Value = ValoresXNeg[i] + x;
+                dgv_7.Rows[i].Cells[1].Value = ValoresY[i] + y;
+                dgv_7.AutoResizeColumns();
+                dgv_7.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                dgv_8.Rows.Add();
+                dgv_8.Rows[i].Cells[0].Value = ValoresY[i] + x;
+                dgv_8.Rows[i].Cells[1].Value = ValoresXNeg[i] + y;
+                dgv_8.AutoResizeColumns();
+                dgv_8.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            }
+
+            for (int i = 0; i < ValoresY.Length; i++)
+            {
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_1.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_1.Rows[i].Cells[1].Value));
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_2.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_2.Rows[i].Cells[1].Value));
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_3.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_3.Rows[i].Cells[1].Value));
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_4.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_4.Rows[i].Cells[1].Value));
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_5.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_5.Rows[i].Cells[1].Value));
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_6.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_6.Rows[i].Cells[1].Value));
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_7.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_7.Rows[i].Cells[1].Value));
+                FormsPlot1.Plot.Add.Line(x, y, Convert.ToDouble(dgv_8.Rows[i].Cells[0].Value), Convert.ToDouble(dgv_8.Rows[i].Cells[1].Value));
+            }
+
+            var c1 = FormsPlot1.Plot.Add.Circle(x, y, radio);
+            c1.LineWidth = 0.4F;
+            c1.LineColor = Colors.Black;
+
+            UnirValoresColumnas(dgv_1);
+            UnirValoresColumnas(dgv_2);
+            UnirValoresColumnas(dgv_3);
+            UnirValoresColumnas(dgv_4);
+            UnirValoresColumnas(dgv_5);
+            UnirValoresColumnas(dgv_6);
+            UnirValoresColumnas(dgv_7);
+            UnirValoresColumnas(dgv_8);
+
+            var puntos = FormsPlot1.Plot.Add.Scatter(listaColumnaX, listaColumnaY);
+            puntos.Color = Colors.Green.WithOpacity(.2);
+            puntos.LineWidth = 0;
+            puntos.MarkerSize = 25;
+
+            FormsPlot1.Plot.Axes.AutoScale();
+            FormsPlot1.Plot.Title("Círculo");
+            FormsPlot1.Refresh();
+        }
+    }
+}
